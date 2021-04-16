960ff8f079e735211e4aa7a4208b672b000a9600
emilydz <edzieweczynski@gmail.com>
2021-04-16 15:17:22 -0500

    Working on sketch

---
+++ script-small.js
@@ -0,0 +1,41 @@
+var canvas;
+let mic;
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(100, 100);
+  canvas.style('z-index', '-1');
+
+  angleMode(DEGREES);
+
+  mic = new p5.AudioIn();
+	mic.start();
+
+  background(255);
+}
+
+
+function draw() {
+  translate(600, 300);
+
+  var micLevel = mic.getLevel();
+  micLevel = mic.getLevel();
+  let y = micLevel;
+
+  let sc = second();
+
+  strokeWeight(1);
+  let fadeSound = map(y, 0, .7, 0, 255)
+  let secondAngle = map(sc, 0, 60, 0, 360);
+  rotate(secondAngle);
+  stroke(255, 0, 0, fadeSound);
+  line(0, 300, 300, 0);
+}
+
+function mouseMoved() {
+	getAudioContext().resume();
+}
+
+function windowResized() {
+  translate(50, 200);
+}
--- script.js
+++ script.js
@@ -8,30 +8,34 @@ function setup() {
 
   angleMode(DEGREES);
 
-  mic = new p5.AudioIn();
-	mic.start();
+  //mic = new p5.AudioIn();
+	//mic.start();
 
   background(255);
 }
 
 
 function draw() {
-  translate(600, 300);
+  translate(400, 300);
 
-  var micLevel = mic.getLevel();
-  micLevel = mic.getLevel();
-  let y = micLevel;
+  //var micLevel = mic.getLevel();
+  //micLevel = mic.getLevel();
+  //let y = micLevel;
 
   let sc = second();
 
-  strokeWeight(1);
-  let fadeSound = map(y, 0, .7, 0, 255)
+  strokeWeight(.2);
+  let x = random(0, 15);
+  //let fadeSound = map(y, 0, .7, 0, 255)
+  let mouseFade =  map(mouseX, 100, 700, 0, 20);
   let secondAngle = map(sc, 0, 60, 0, 360);
   rotate(secondAngle);
-  stroke(255, 0, 0, fadeSound);
+  stroke(255, 0, 0, mouseFade);
   line(0, 300, 300, 0);
+
+  console.log(mouseX);
 }
 
-function mouseMoved() {
-	getAudioContext().resume();
-}
+//function mouseMoved() {
+	//getAudioContext().resume();
+//}
